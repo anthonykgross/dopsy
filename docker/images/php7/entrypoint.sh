41d298e513a76d2f23a6ce6aa8f2baa4b51ab3e3
Anthony K Gross <anthony.k.gross@pepperstone.com>
2017-04-27 18:46:53 +1000

    Debug images

--- docker/images/php7/entrypoint.sh
+++ docker/images/php7/entrypoint.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 set -e
 
-rm node_modules/ -Rf
+install() {
+    rm node_modules/ -Rf
+    npm install
+    gulp
+    php composer.phar self-update
+    php composer.phar install
+    #php app/console assets:install
+}
 
-npm install
-gulp 
+tests() {
+    php bin/phpunit -c app/
+}
 
-php composer.phar self-update
-php composer.phar install
+run() {
+    chmod 777 * -Rf
+    supervisord
+}
 
-#php app/console assets:install
-
-chmod 777 * -Rf
-
-supervisord
+case "$1" in
+"install")
+    echo "Install"
+    install
+    ;;
+"tests")
+    echo "Tests"
+    tests
+    ;;
+"run")
+    echo "Run"
+    run
+    ;;
+*)
+    echo "Custom command : $@"
+    exec "$@"
+    ;;
+esac
