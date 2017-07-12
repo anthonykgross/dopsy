df40e468e86c36b32bc34618703ef4e441bbcbd7
Anthony K Gross <anthony.k.gross@pepperstone.com>
2017-07-13 09:38:03 +1000

    Makefile

--- create-images.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-docker build --file="docker/images/php7/Dockerfile" --tag="dopsy_php7:latest" docker/images/php7/.
-docker build --file="docker/images/mysql/Dockerfile" --tag="dopsy_mysql:latest" docker/images/mysql/.
--- install.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-chmod -Rf 777 docker/
-chmod 644 docker/etc/mysql/my.cnf
